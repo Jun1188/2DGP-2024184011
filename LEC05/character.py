4ec674fe13009dd4d74b533d2d54ae6ef62be1ea
junyoung365 <135by135by@gmail.com>
2026-09-22 16:41:46 +0900

    fix setting and update char.py
    python 버전충돌로 인한 pico2d import 문제 해결
    pico2d canvas 출력
    triangle 함수 추가 및 loop 조건 변경

--- LEC05/character.py
+++ LEC05/character.py
@@ -6,22 +6,26 @@ def move_circle():
 def move_rect():
     print("rectangle")
     return
+def move_triangle():
+    print("tirangle")
+    return
 
-#open_canvas(800, 600)
+open_canvas(800, 600)
 
 # 여기를 채우시오.
 
-#character = load_image('character.png')
-# grass = load_image('grass.png')
-#clear_canvas()
+character = load_image('character.png')
+grass = load_image('grass.png')
+clear_canvas()
 
-
-while True:
+loop = 5
+while loop > 0:
     move_circle()
     move_rect()
-    #move_triangle() 
+    move_triangle() 
+    loop -= 1
     #코딩 고수는 함수를 만들고 나중에 호출한다 : pseudo code
     pass #아무것도 하지 않고 통과함
 
-#close_canvas()
+close_canvas()
 
