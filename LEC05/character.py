fc20d57393a03c2ef087dd3decb1770807a445e0
junyoung365 <135by135by@gmail.com>
2026-09-22 16:50:35 +0900

    update char.py - move
    움직임에 필요한 변수 활용과 draw, canvas관련 함수, delay에 대해 배워보고,
    test_move로 unit test해본다

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,5 +1,18 @@
 from pico2d import *
 
+def test_move(x, y):
+    
+    delta = 10
+
+    x += delta
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    update_canvas()
+    delay(0.1)
+    return x, y
+
+
 def move_circle():
     print("circle")
     return
@@ -18,14 +31,17 @@ character = load_image('character.png')
 grass = load_image('grass.png')
 clear_canvas()
 
-loop = 5
+loop = 10
+x = 0
+y = 90
 while loop > 0:
-    move_circle()
-    move_rect()
-    move_triangle() 
+    x, y = test_move(x, y)
+    #move_circle()
+    #move_rect()
+    #move_triangle() 
     loop -= 1
     #코딩 고수는 함수를 만들고 나중에 호출한다 : pseudo code
-    pass #아무것도 하지 않고 통과함
+    #pass #아무것도 하지 않고 통과함
 
 close_canvas()
 
