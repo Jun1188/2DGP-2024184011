cbc8529c9da1711a484d17f1a411815fab57060d
junyoung365 <135by135by@gmail.com>
2026-09-22 19:07:40 +0900

    Create unit_test.py
    원 운동을 위한 unit test 및 print 확인

---
+++ LEC05/unit_test.py
@@ -0,0 +1,12 @@
+﻿import math    
+x = 400
+y = 300
+r = 100
+degree = 0
+delta = 10
+while degree < 360:
+    degree += delta
+    radian = math.radians(degree)
+    x = 400 + r * math.cos(radian)
+    y = 300 + r * math.sin(radian)
+    print(f"{x}, {y}")
