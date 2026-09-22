82d89f323426a34d766dcf3d281a4b82a101a32d
junyoung365 <135by135by@gmail.com>
2026-09-22 16:54:43 +0900

    update char.py - 경계선 이동
    canvas 경계선을 따라 이동한다

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,24 +1,23 @@
 from pico2d import *
 
-def test_move(x, y):
+
+def RectMove():
+    x = 0
+    y = 90
+    delta = 20
+    while x < 800:
+        x += delta
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw(x, y)
+        update_canvas()
     
-    delta = 10
-
-    x += delta
-    clear_canvas()
-    grass.draw(400, 30)
-    character.draw(x, y)
-    update_canvas()
-    delay(0.1)
-    return x, y
-
+        delay(0.01)
 
 def move_circle():
     print("circle")
     return
-def move_rect():
-    print("rectangle")
-    return
+
 def move_triangle():
     print("tirangle")
     return
@@ -35,10 +34,8 @@ loop = 10
 x = 0
 y = 90
 while loop > 0:
-    x, y = test_move(x, y)
-    #move_circle()
-    #move_rect()
-    #move_triangle() 
+   
+    RectMove()
     loop -= 1
     #코딩 고수는 함수를 만들고 나중에 호출한다 : pseudo code
     #pass #아무것도 하지 않고 통과함
