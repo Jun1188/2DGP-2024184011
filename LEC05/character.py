bb807780330a90941e73c95b71d1c6f96960665a
junyoung365 <135by135by@gmail.com>
2026-09-22 16:32:12 +0900

    update char.py - 함수 정의와 무한루프
    함수를 정의하고 호출 무한루프를 print로 확인한다

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,20 +1,27 @@
 from pico2d import *
 
+def move_circle():
+    print("circle")
+    return
+def move_rect():
+    print("rectangle")
+    return
 
-open_canvas(800, 600)
+#open_canvas(800, 600)
 
 # 여기를 채우시오.
 
-character = load_image('character.png')
+#character = load_image('character.png')
 # grass = load_image('grass.png')
-clear_canvas()
+#clear_canvas()
+
 
 while True:
-    #move_circle()
-    #move_rect()
+    move_circle()
+    move_rect()
     #move_triangle() 
     #코딩 고수는 함수를 만들고 나중에 호출한다 : pseudo code
     pass #아무것도 하지 않고 통과함
 
-close_canvas()
+#close_canvas()
 
